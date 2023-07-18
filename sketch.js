0b5d0a4c00ed2df10f61bc7f9c1e9fa32f0aa31c
DesertFool <139814639+DesertFool@users.noreply.github.com>
2023-07-18 05:02:41 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,20 +1,17 @@
 function setup() {
-    createCanvas (500, 500);
-    background (120, 120, 120);
-    strokeWeight(3)
-    stroke(255)
-    point (20, 20);
-    line (100, 100, 400, 400);
+    createCanvas(1500, 950);
+    background(0, 0, 0);
+    noStroke();
+    
+ }
 
-    fill(255, 0, 0)
-    ellipse(180, 200, 200, 100);
+ function draw(){
+    ellipse(mouseX, mouseY, 20, 20);
+ }
 
-    fill(0, 255, 0)
-    rect (200, 200, 120, 190);
-
-    fill(255, 255, 0)
-    triangle (50, 450, 200, 450, 125, 320);
-
-    fill(0, 0, 255)
-    quad (340, 50, 300, 120, 430, 155, 450, 20);
-}
+ function mouseClicked(){
+    let r = random(0, 255);
+    let g = random(0, 255);
+    let b = random(0, 255);
+    fill(r, g, b);
+ }
